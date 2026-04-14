72d702d2f30f30518dc4fb37cfcddf09c32a9b70
Rafael Gomes <rafaelmgomes.dev@gmail.com>
2026-04-13 22:09:51 -0300

    5. Classe TestSuite

---
+++ examples/apoio.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import sys
+
+PROJECT_ROOT = Path(__file__).resolve().parent.parent
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.insert(0, str(PROJECT_ROOT))
+
+from xunit import TestCase
+
+
+class TestStub(TestCase):
+    def test_success(self):
+        assert True
+
+    def test_failure(self):
+        assert False
+
+    def test_error(self):
+        raise Exception
+
+
+class TestSpy(TestCase):
+    def __init__(self, name):
+        TestCase.__init__(self, name)
+        self.was_run = False
+        self.was_set_up = False
+        self.was_tear_down = False
+        self.log = ""
+
+    def set_up(self):
+        self.was_set_up = True
+        self.log += "set_up "
+
+    def test_method(self):
+        self.was_run = True
+        self.log += "test_method "
+
+    def tear_down(self):
+        self.was_tear_down = True
+        self.log += "tear_down"
--- xunit/__init__.py
+++ xunit/__init__.py
@@ -1,4 +1,5 @@
 from .test_case import TestCase
 from .test_result import TestResult
+from .test_suite import TestSuite
 
-__all__ = ["TestCase", "TestResult"]
+__all__ = ["TestCase", "TestResult", "TestSuite"]
