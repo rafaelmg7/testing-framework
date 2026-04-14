ba77a006642ec6064e94b7bf579c411789164ce2
Rafael Gomes <rafaelmgomes.dev@gmail.com>
2026-04-13 22:12:08 -0300

    6. Classes TestLoader e TestRunner

--- xunit/__init__.py
+++ xunit/__init__.py
@@ -1,5 +1,7 @@
 from .test_case import TestCase
+from .test_loader import TestLoader
 from .test_result import TestResult
+from .test_runner import TestRunner
 from .test_suite import TestSuite
 
-__all__ = ["TestCase", "TestResult", "TestSuite"]
+__all__ = ["TestCase", "TestResult", "TestSuite", "TestLoader", "TestRunner"]
